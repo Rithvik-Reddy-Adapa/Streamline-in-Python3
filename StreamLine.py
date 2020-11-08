d176a32518af934a24523c5e4165598b17c4b717
Rithvik-Reddy-Adapa <72334064+Rithvik-Reddy-Adapa@users.noreply.github.com>
2020-11-08 10:00:19 +0530

    Update StreamLine.py

--- StreamLine.py
+++ StreamLine.py
@@ -1,3 +1,21 @@
+'''
+    Streamline in Python3
+    Copyright (C) Year: 2020,  Author: Rithvik Reddy Adapa
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 def streamline(*,x,y,z=None,u,v,w=None,xi,yi,zi=None,n,length,r_mag=False,r_u=False,r_v=False,r_w=False):
     '''
     #   Made on 23 / 07 / 2020 (dd / mm / yyyy)
